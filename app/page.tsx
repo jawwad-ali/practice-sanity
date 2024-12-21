ec896468a6c32e449aab9e6aa5d76f379ead3a2a
Ali jawwad <33836051+jawwad-ali@users.noreply.github.com>
2024-12-21 23:57:25 +0500

    remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Link from "next/link"
 import { client } from "../sanity/lib/client"
 import { urlFor } from "@/sanity/lib/image"
-
-interface IBooks {
-  id: number
-  name: string
-  type: string
-  available: boolean
-}
 
 const getPets = async () => {
   // const query = await client.fetch(
@@ -19,24 +11,11 @@ const getPets = async () => {
 }
 
 export default async function Home() {
-  const url = await fetch("https://simple-books-api.glitch.me/books")
-  const res = await url.json()
-
   const listPets = await getPets()
   console.log(listPets)
 
-
   return (
-
-
     <main className="flex flex-col py-3 text-center items-center justify-center">
-
-      {res.map((book: IBooks, index: number) => (
-
-        <Link key={index} href={`/book/${book.id}`} className="py-5">{(book.name)}</Link>
-
-      ))}
-
       <h3 className="text-5xl">PETS</h3>
       {listPets.map((pet: any, index: number) => (
         <div key={index}>
